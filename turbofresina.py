745f4e2209e8810b8e8928bd111214fd1efc282a
Hyd3L <hydel@linuxmail.org>
2018-05-11 00:49:58 +0200

    This looks like a working script

--- turbofresina.py
+++ turbofresina.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
-    T.U.R.B.O.F.R.E.S.A.
+    T.U.R.B.O.F.R.E.S.I.N.A
     Turboaggeggio Utile alla Rimorzione di Byte Obrobriosi e di abominevoli
     File da dischi rigidi Riciclati ed altri Elettronici Sistemi di
-    Accumulazione (semi)permanente di dati.
+    Immagazzinamento (semi)permanente di dati Necessariamente Automatizzato.
     Copyright (C) 2018  Hyd3L
 
     This program is free software: you can redistribute it and/or modify
@@ -22,45 +22,38 @@
 
 import os
 import sys
-import getpass
 import datetime
 import threading
 import subprocess
+import argparse  # @quel_tale will you finally stop smashing my nuts?
+from getpass import getuser
 
 # Path of the log file
-LOG_PATH = '/home/' + getpass.getuser() + '/.local/share/turbofresa/log.txt'
+LOG_PATH = '/home/' + getuser() + '/.local/share/turbofresa/log.txt'
 
 # Verbosity levels
 ERROR, WARNING, INFO = range(0, 3)
 VERBOSITY = INFO
 
 
-# Function that returns a formatted string with current time
 def now():
+	"""
+		:return: a formatted string containing the current date and time
+	"""
 	return datetime.datetime.now().strftime('[%Y-%m-%d %H:%M:%S] ')
 
 
-"""
-	Console : class for printing errors, warnings and info messages
-				to the stdout and into the log file.
-
-	@member printLevel : verbosity level (ERROR, WARNING or INFO)
-	@member logFile    : the log file pointer
-
-	@method info : prints info msg to stdout and to the logFile
-	@method warn : prints warning to stdout and to the logFile
-	@method err  : prints error to stdout and to the logFile
-"""
-
-
 class Console(object):
+	"""
+		Class that handles the log file.
+	"""
 	def __init__(self):
 		self.printLevel = VERBOSITY
 		try:
 			self.logFile = open(LOG_PATH, "a")
 		except IOError:
 			print("error: Permission denied. Log file couldn't be created.")
-			sys.exit(1)
+			sys.exit(34)
 
 	def info(self, msg, to_std_out=True):
 		if self.printLevel >= INFO:
@@ -68,10 +61,10 @@ class Console(object):
 				print("info: " + msg)
 			self.logFile.write(now() + "info: " + msg + '\n')
 
-	def proc(self, msg, to_std_out=True):
+	def task(self, msg, to_std_out=True):
 		if to_std_out is True:
-			print("proc: " + msg)
-		self.logFile.write(now() + "proc: " + msg + '\n')
+			print("task: " + msg)
+		self.logFile.write(now() + "task: " + msg + '\n')
 
 	def warn(self, msg, to_std_out=True):
 		if self.printLevel >= WARNING:
@@ -92,3 +85,89 @@ class Console(object):
 # Initialize log
 log = Console()
 
+
+def secure_exit(status=0):
+	log.exit()
+	sys.exit(status)
+
+
+def detect_os() -> str:
+	"""
+		Detects the hard drives connected to the machine
+		:return: a list containing '/dev/sdX' entries
+	"""
+	os_disk = os.popen('df | grep /dev/sd | cut -b -8').read().split('\n')[0]
+	return os_disk
+
+
+def detect_disks() -> list:
+	"""
+		Detects the hard drives connected to the machine
+		:return: a list containing '/dev/sdX' entries
+	"""
+	os_disk = detect_os()
+	disks = list()
+	lsblk = os.popen('lsblk | grep sd').read()
+	for line in lsblk:
+		if line.startswith('s'):
+			path = '/dev/'+line[0]
+			if path == os_disk:
+				print("Skipping mounted drive: " + path)
+				continue
+			disks.append(path)
+	return disks
+
+
+class Task(threading.Thread):
+	"""
+		Class that handles the cleaning operation of a disk
+	"""
+
+	def __init__(self, hdd):
+		"""
+			:param hdd: A '/dev/sdX' formatted string
+		"""
+		threading.Thread.__init__(self)
+		self.disk_path = hdd
+		self.drive_name = hdd.split('/')[2]
+
+	def run(self):
+		log.task("Started cleaning %s" % self.drive_name)
+		subprocess.run(['sudo', 'badblocks', '-w', '-t', '0x00', '-o', self.drive_name, self.disk_path])
+		result = os.popen('cat %s' % self.drive_name).read()
+		if result == "":
+			log.task("%s successfully cleaned.")
+			subprocess.run(['rm', self.drive_name])
+		else:
+			log.task("%s is broken.")
+
+
+def main():
+	parser = argparse.ArgumentParser(description='Automatically drill every single connected hard drive.')
+	parser.add_argument('-s', '--shutdown', help="Shutdown the machine when everything is done.")
+	parser.set_defaults(shutdown=False)
+	args = parser.parse_args()
+
+	print("===> Detecting connected hard drives.")
+	hdds = detect_disks()
+	tasks = list()
+
+	for d in hdds:
+		tasks.append(Task(d))
+
+	print("===> Cleaning disks")
+	for t in tasks:
+		t.start()
+
+	for t in tasks:
+		t.join()
+
+	if args.shutdown is True:
+		log.info("System halted by the user.")
+		subprocess.run(['shutdown', '+1'])
+	print("Done.")
+
+
+if __name__ == '__main__':
+	main()
+	secure_exit()
