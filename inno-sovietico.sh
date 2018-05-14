b68d706c83b4e0c7515c21d3cb4095353b377cf2
Hyd3L <hydel@linuxmail.org>
2018-05-14 19:09:16 +0200

    Soviet hymn

---
+++ inno-sovietico.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+beep -l 375 -f 392 #sol
+beep -l 750 -f 523 #do5
+beep -l 463 -f 392 #sol
+beep -l 187 -f 440 #la
+beep -l 750 -f 494 #si
+beep -l 375 -f 330 #mi
+beep -l 375 -f 330 #mi
+beep -l 750 -f 440 #la
+beep -l 463 -f 392 #sol
+beep -l 187 -f 349 #fa
+beep -l 750 -f 392 #sol
+beep -l 463 -f 262 #do4
+beep -l 187 -f 262 #do4
+beep -l 750 -f 294 #re
+beep -l 463 -f 294 #re
+beep -l 187 -f 330 #mi
+beep -l 750 -f 349 #fa
+beep -l 463 -f 349 #fa
+beep -l 187 -f 392 #sol
+beep -l 750 -f 440 #la
+beep -l 375 -f 494 #si
+beep -l 375 -f 523 #do5
+beep -l 1125 -f 587 #re5
+
