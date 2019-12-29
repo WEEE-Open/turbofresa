f21d1d65a3b69d6305d022b9b3c983e573fc33dd
Yaku <lorenzo.deluca2407@gmail.com>
2019-12-29 21:42:52 +0100

    Changed filegen so it generates output folder

--- smartctl_filegen.sh
+++ smartctl_filegen.sh
@@ -9,13 +9,17 @@ else
     echo -n "Unexpected number of parameters.\nUsage: sudo ./generate_files.sh /optional/path/to/files"
 fi
 
+[ ! -d $OUTPATH ] && mkdir $OUTPATH
+
 # removing all old smartctl files before writing new ones
 rm -f "$OUTPATH"/smartctl-dev-*.txt
 
 DISKZ=($(lsblk -d -I 8 -o NAME -n))
 echo Found $((${#DISKZ[@]}-1)) disks # count reduced by 1 to ignore sda
 for d in "${DISKZ[@]}"; do
-  if [ $d != "sda" ]; then # skips sda to avoid wiping system data
+  #if [ $d != "sda" ]; then # skips sda to avoid wiping system data
 	  smartctl -x /dev/$d > "$OUTPATH/smartctl-dev-$d.txt"
-	fi
+	#fi
 done
+
+exit 0
