d883d7d2a58b22cdd82e6b9a0bee6f0b3c186a52
Yaku <lorenzo.deluca2407@gmail.com>
2020-04-16 13:24:52 +0200

    Better conflict handling on TARALLO

--- turbofresa.py
+++ turbofresa.py
@@ -38,6 +38,8 @@ __version__ = '1.3'
 quiet = None
 simulate = None
 
+tarallo_instance = None
+
 
 def ask_confirm():
     """
@@ -51,6 +53,7 @@ def ask_confirm():
             exit(0)
         else:
             print("Unrecognized response... Asking again nicely.")
+
 
 class Task(Process):
     """
@@ -74,15 +77,20 @@ class Task(Process):
         and the broken hard drive is reported into the log file and informations
         are written to the T.A.R.A.L.L.O. database.
         """
-        exit_code = sp.Popen(['sudo', 'badblocks', '-w', '-t', '0x00', '-o', self.disk.code, self.disk.dev]).returncode
+        exit_code = sp.Popen(['sudo', 'badblocks', '-w', '-t', '0x00', '-o',
+                              self.disk['code'], "/dev/"+self.disk['mount_point']]).returncode
         # result = os.popen('cat %s' % self.disk.code).read()
         # if result == "":
         if exit_code != 0:
-            sp.run(['rm', '-f', self.disk.code])
+            sp.run(['rm', '-f', self.disk["code"]])
+            return True
         else:
             # TODO: Write on tarallo that the hard drive is broken
             # Write it in the turbofresa log file as well
-            pass
+            global tarallo_instance
+            disk['features']['smart-data'] = smartctl_parser.SMART.fail
+            add_to_tarallo_broken(tarallo_instance, disk['features'])
+            return False
 
 
 def add_to_tarallo(instance: Tarallo.Tarallo, disk: dict) -> bool:
@@ -90,11 +98,38 @@ def add_to_tarallo(instance: Tarallo.Tarallo, disk: dict) -> bool:
     Adds disk to Tarallo database
     :param instance: Tarallo instance where to add the disk
     :param disk: disk to add to the database
-    :return: True if added successfully, False otherwise
+    :return: True if added successfully or it was already present,
+        False if there were multiple instances of it in the database
     """
+
+    print("===> Searching the T.A.R.A.L.L.O. databse for disk with serial number {}".format(disk['sn']))
+    disk_code = instance.get_codes_by_feature('sn', disk['sn'])
+
+    if len(disk_code) > 1:
+        print("Multiple disks in the database corresponding to the serial number: " + disk['sn'])
+        print("Won't proceed until conflict is solved")
+        return False
+    elif len(disk_code) == 1:
+        print(f"Disk with serial number {disk['sn']} already present in the database"
+              f"with the code {disk_code}")
+        item = instance.get_item(disk_code)
+        for key, value in item.features.items():
+            if key == 'smart_data' or key == 'smart_data_long':
+                continue
+            if value != disk[key]:
+                print("There's a conflict in the database for this disk")
+                print("Won't proceed until conflict is solved")
+                return False
+            print("The entry doesn't conflict with the current disk, proceeding anyway")
+            return True
+    elif len(disk_code) == 0:
+        print("No corresponding disk in the database")
+        print("===> Adding disk to the database")
+
     item = Item.Item()
     item.features = disk
     item.location = 'Magazzino'  # TODO: maybe it can be set from config or a better default should be picked
+
     try:
         instance.add_item(item=item)
         print("Item inserted successfully")
@@ -102,6 +137,27 @@ def add_to_tarallo(instance: Tarallo.Tarallo, disk: dict) -> bool:
         print("Item not inserted")
         response = instance.response
         print("HTTP status code:", response.status_code, "\n" + response.json()['message'])
+        return False
+
+    print("Successfully added the disk")
+    print("Disk code on the Database: " + instance.get_codes_by_feature('sn', disk['sn']))
+    return True
+
+
+def add_to_tarallo_broken(instance: Tarallo.Tarallo, disk: dict) -> bool:
+    if disk['smart-data'] != smartctl_parser.SMART.fail:
+        print("Not a broken disk")
+        return False
+
+    if add_to_tarallo(instance, disk) is False:
+        print("Failed to update informations")
+        return False
+
+    disk_code = instance.get_codes_by_feature('sn', disk['sn'])
+    try:
+        instance.update_features(disk_code, disk)
+    except Errors.ValidationError:
+        print("Failed to update informations")
         return False
 
     return True
@@ -112,41 +168,38 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--shutdown', action='store_true', help='Shutdown the machine when everything is done.')
     parser.add_argument('-q', '--quiet', action='store_true', help='Run in background and suppress stdout.')
     parser.add_argument('-d', '--dry', action='store_true', help='Launch simulation.')
+    parser.add_argument('--usb', action='store_true', help='Launch simulation.')
     parser.add_argument('--version', '-V', action='version', version='%(prog)s v.' + __version__)
     parser.set_defaults(shutdown=False)
     parser.set_defaults(quiet=False)
     parser.set_defaults(dry=False)
+    parser.set_defaults(usb=False)
     args = parser.parse_args()
     quiet = args.quiet
     simulate = args.dry
 
+    global tarallo_instance
+
     # ask_confirm()
     if not quiet:
         print("===> Detecting connected hard drives.")
-    disks = smartctl_parser.main()
+
+    disks = smartctl_parser.parse_disks(interactive=True, usbdebug=args.usb)
     tasks = []
 
     # Tarallo connection
     load_dotenv()
-    instance = Tarallo.Tarallo(os.getenv("TARALLO_URL"), os.getenv("TARALLO_TOKEN"))
+    tarallo_instance = Tarallo.Tarallo(os.getenv("TARALLO_URL"), os.getenv("TARALLO_TOKEN"))
 
     # Adding disks to clean in queue and adding them to T.A.R.A.L.L.O if not present
     for d in disks:
-        print("===> Searching the T.A.R.A.L.L.O. databse for disk with serial number {}".format(d['sn']))
-        disk_code = instance.get_codes_by_feature('sn', d['sn'])
-        if len(disk_code) > 1:
-            print("Multiple disks in the database corresponding to the serial number: " + d['sn'])
-            print("Won't proceed to disk wiping until conflict is solved")
-            continue
-        elif len(disk_code) == 0:
-            print("No corresponding disk in the database")
-            print("===> Adding disk to the database")
-            if not add_to_tarallo(instance=instance, disk=d):
-                print("Skipping wiping of this disk")
-                continue
-        elif len(disk_code) == 1:
-            print("Disk already present in the database")
+        disk = d['features']
 
+        if add_to_tarallo(tarallo_instance, disk) is False:
+            print("Something went wrong with Disk addition to database, skipping to the next one")
+            continue
+
+        d['code'] = tarallo_instance.get_codes_by_feature('sn', disk['sn'])
         tasks.append(Task(d))
         
     if not quiet:
